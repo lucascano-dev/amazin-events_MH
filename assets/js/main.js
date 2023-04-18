30da90b0cae16c34ebb0e1d481790b6ebb06713e
Lucas Cano <lucas_cano@outlook.com>
2023-04-18 15:06:41 -0300

    Fixed: footer(CSS)/ EventListerner input: main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ const accessData = fetch(myApiURL)
   .then((data) => {
     theCurrentDate = data.currentDate;
     myEvents = retriveEvents([...data.events]);
+
     //recupero los eventos en myEvents
     // console.log(retriveEvents([...data.events]));        //TODOS
     // console.log(retriveEvents([...data.events],true));   //PASADOS
@@ -14,8 +15,27 @@ const accessData = fetch(myApiURL)
 
     // PASO 3: PINTAR LAS CATEGORIAS 
     paintCategorys(categorias);
-      // PASO 3: PINTAR LAS CARDS 
+    // PASO 3: PINTAR LAS CARDS 
     paintCards(myEvents)
 
   });
+
+
+
+//Evento de escucha para input
+
+let buscador = document.getElementById("inputSearch"); //captura id=inputSarch
+
+buscador.addEventListener('keyup', () => { //keydown para filtrar cuando presione una tecla
+  //Si filtro tiene valor desde input, entonces vuelve a crear el html
+  let eventosFiltrados = myEvents.filter((miEvento) => {
+
+    return miEvento.name.toLowerCase().includes(buscador.value.toLowerCase()); //si le pongo llaves entonces debo usar return, caso contrario lo puedo hacer una sola linea al lado de la funcion flecha
+  });
+
+  console.log(eventosFiltrados);
+  paintCards(eventosFiltrados);
+
+
+})
 
